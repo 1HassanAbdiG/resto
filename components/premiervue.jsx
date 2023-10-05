be9f2b3a2b4daa6c08152b2e18db3b3ae2191fc5
1HassanAbdi <hassanabdigaleb@gmail.com>
2023-10-05 15:50:22 -0400

    resto 2er commit

--- components/premiervue.jsx
+++ components/premiervue.jsx
@@ -9,14 +9,17 @@ export default function PremierVue() {
     return <section className={styles.partie1} id="panier">
         <div className={styles.contenu}>
             <h2>Que Des Plats Délicieux</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis numquam distinctio, voluptate
-                veritatis vel ipsum animi ex eligendi harum a debitis laboriosam possimus placeat ad officia commodi at!
-                Quo, aliquid.</p>
-
-            {!user && <Link href="/inscription" className={styles.btn1}> Inscription</Link>}
-
-            <Link href="/connection" className={styles.btn2}>Connection</Link>
-            <a href="#menu" className={styles.btn3}>Voir Menu</a>
+            <p>Notre restaurant vous propose une expérience gastronomique unique. 
+                Découvrez notre menu varié, composé de plats savoureux préparés avec des ingrédients frais et de qualité.
+                 Que vous soyez amateur de cuisine traditionnelle ou que vous préfériez les saveurs exotiques, 
+                 notre équipe de chefs talentueux saura combler vos papilles. 
+                 Venez savourer un repas inoubliable dans un cadre chaleureux et convivial.</p>
         </div>
-    </section>
+
+        {!user && <Link href="/inscription" className={styles.btn1}> Inscription</Link>}
+
+        <Link href="/connection" className={styles.btn2}>Connection</Link>
+        <a href="#menu" className={styles.btn3}>Voir Menu</a>
+    </div >
+    </section >
 }
